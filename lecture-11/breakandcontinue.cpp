32e7faa3011586bec305042a4811ea792d978637
rutugoti <rutugotiwork24@gmail.com>
2026-02-11 13:41:55 +0530

    lecture-11 complete

---
+++ lecture-11/breakandcontinue.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    // int i=0;
+    // int n=10;
+
+    // while(i<=10){
+
+    //     if(i==5 )// break on 5
+    //     break;
+
+    //     cout << i<<" ";
+    //     i++;
+    // }
+
+     int i=0;
+    int n=10;
+
+    while(i<=10){
+
+        if(i%2==0 )// break on 5
+        continue;
+
+        cout << i<<" ";
+        i++;
+    }
+
+
+}
---
+++ lecture-11/doWhileLoop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+// rare use most used are for and while 
+//it run one time no matter what is the condition
+    int i=1;
+    do{
+        i++;
+        cout <<i;
+    }while(i<=10);
+}
