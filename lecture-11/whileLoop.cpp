701ea5934d33a2d3671a9ce2efd22eedd4e3ac73
rutugoti <rutugotiwork24@gmail.com>
2026-02-11 13:24:53 +0530

    while loop example

---
+++ lecture-11/whileLoop.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // table of 10
+    //  int n = 10;;
+    //  int i=1;
+    //  while(i<=n){
+
+    //     cout <<n*i<<" ";
+    //     i++;
+
+    // }
+
+    // factor of a no
+
+    //  int n = 10;;
+    // int i=1;
+    // while(i<n){
+
+    //     if(n%i==0)
+
+    //     cout <<i<<" ";
+    //     i++;
+
+    // }
+
+    // print even no
+    // int n = 20;
+    // ;
+    // int i = 1;
+    // while (i < n)
+    // {
+
+    //     if (i % 2 == 0)
+
+    //         cout << i << " ";
+    //     i++;
+    // }
+
+
+    
+}
