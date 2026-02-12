76dcdf66e0889cd63f3efdd7d75216e89cc9fc4f
rutugoti <rutugotiwork24@gmail.com>
2026-02-12 08:58:15 +0530

    lecture-13 done

--- lecture-13/GFG_P_Squarechessboard.cpp
+++ lecture-13/GFG_P_Squarechessboard.cpp
@@ -1,13 +1,13 @@
-class Solution {
-public:
-    long long squaresInChessBoard(long long N) {
-        long long ans = 0; // start from 0, not 1
+// class Solution {
+// public:
+//     long long squaresInChessBoard(long long N) {
+//         long long ans = 0; // start from 0, not 1
         
-        for(long long i = N; i > 0; i--) { // loop control should be i, not N
-            long long num = i * i;          // use long long to avoid overflow
-            ans += num;
-        }
+//         for(long long i = N; i > 0; i--) { // loop control should be i, not N
+//             long long num = i * i;          // use long long to avoid overflow
+//             ans += num;
+//         }
         
-        return ans;
-    }
-};
+//         return ans;
+//     }
+// };
