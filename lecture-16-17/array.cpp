a208d18eb6490b2fef0cb561f767d540b14360af
rutugoti <rutugotiwork24@gmail.com>
2026-02-14 09:09:00 +0530

    lecture-16 complete

---
+++ lecture-16-17/array.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+#include <climits>
+
+int main(){
+
+    //int arr[5]  = {2,3,4,5,6};
+
+    // int arr[5];
+
+    //  for(int i=0;i<5;i++){
+
+    //     cin>>arr[i];
+    // }
+
+    // int arr[5]={2,3,4};
+    //     for(int i=0;i<5;i++){
+
+    //     cout<<arr[i];
+    // }
+
+
+    // int arr[5]={2,3,5,6,4};
+
+    // int ans = INT_MAX;
+    //     for(int i=0;i<5;i++){
+
+    //    if(arr[i]<ans){
+    //     ans = arr[i];
+    //    }
+
+    // }
+    // cout <<ans;
+
+    //max value
+    //  int arr[5]={2,3,5,6,4};
+
+    // int ans = INT_MIN;
+    //     for(int i=0;i<5;i++){
+
+    //    if(arr[i]>ans){
+    //     ans = arr[i];
+    //    }
+
+    // }
+    // cout <<ans;
+
+}
---
+++ lecture-16-17/arrayHW.cpp
@@ -0,0 +1,140 @@
+#include<iostream>
+using namespace std;
+#include <algorithm>
+#include<climits>
+
+
+int main(){
+
+//    1: Take 20 elements from user input and find its sum with the help of an array.
+
+    // int arr[20] ;
+    // int ans = 0;
+
+    // for(int i =0;i<20;i++){
+    //     cin>>arr[i];
+    // }
+
+    //  for(int i =0;i<20;i++){
+    //    ans = ans+arr[i];
+    // }
+    // cout << ans;
+
+
+
+
+    // int n;
+    // cout << "Enter size of array: ";
+    // cin >> n;
+
+    // int arr[n];   // array of user-defined size
+
+    // cout << "Enter " << n << " elements:\n";
+    // for(int i = 0; i < n; i++) {
+    //     cin >> arr[i];
+    // }
+
+    // int key;
+    // cout << "Enter element to search: ";
+    // cin >> key;
+
+    // int index = -1;   // default if not found
+
+    // for(int i = 0; i < n; i++) {
+    //     if(arr[i] == key) {
+    //         index = i;
+    //         break;    // stop when found
+    //     }
+    // }
+
+    // cout << "Index: " << index;
+
+
+
+
+
+  //   Calculate the average of elements in an array of size 18.
+
+  
+    // int arr[18] ;
+    // int ans = 0;
+
+    // for(int i =0;i<18;i++){
+    //     cin>>arr[i];
+    // }
+
+    //  for(int i =0;i<18;i++){
+    //    ans = ans+arr[i];
+    // }
+    // cout << ans/18;
+
+
+    
+//4: Create an array of char types and store ‘a’ to ‘z’ in it. Then print the element of the arrays
+
+    // char arr[26];
+
+    // for(int i=0;i<26;i++){
+    //     cin>>arr[i];
+    // }
+
+    // for(int i=0;i<26;i++){
+    //     cout << arr[i] <<" ";
+    // }
+
+
+   // 5: Find the second largest element in an array of unique elements of size n. Where n>3.
+
+;
+
+    // int arr[] = {7, 2, 9, 4, 6};
+    // int n = 5;   // n > 3
+
+    // int largest = arr[0];
+    // int secondLargest = INT_MIN;
+
+    // for(int i = 1; i < n; i++) {
+    //     if(arr[i] > largest) {
+    //         secondLargest = largest;
+    //         largest = arr[i];
+    //     }
+    //     else if(arr[i] > secondLargest) {
+    //         secondLargest = arr[i];
+    //     }
+    // }
+
+    // cout << "Second Largest: " << secondLargest << endl;
+
+
+
+//6: Find the third smallest element in an array of unique elements size n. Where n>3.
+
+
+    // int arr[] = {7, 2, 9, 4, 6};
+    // int n = 5;   // n > 3
+
+    // int smallest = arr[0];
+    // int secondSmallest = INT_MAX;
+    // int thirdSmallest = INT_MAX;
+
+    // for(int i = 1; i < n; i++) {
+
+    //     if(arr[i] < smallest) {
+    //         thirdSmallest = secondSmallest;
+    //         secondSmallest = smallest;
+    //         smallest = arr[i];
+    //     }
+    //     else if(arr[i] < secondSmallest) {
+    //         thirdSmallest = secondSmallest;
+    //         secondSmallest = arr[i];
+    //     }
+    //     else if(arr[i] < thirdSmallest) {
+    //         thirdSmallest = arr[i];
+    //     }
+    // }
+
+    // cout << "Third Smallest: " << thirdSmallest;
+
+
+
+}
