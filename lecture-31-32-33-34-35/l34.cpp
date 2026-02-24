7fc239536880279250adc8bcfcdbb7ba44b72556
rutugoti <rutugotiwork24@gmail.com>
2026-02-24 07:17:43 +0530

    lecture-34 done

---
+++ lecture-31-32-33-34-35/l34.cpp
@@ -0,0 +1,28 @@
+
+//https://leetcode.com/problems/search-a-2d-matrix/description/
+
+
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int n=matrix.size();
+//         int m=matrix[0].size();
+
+//         int start=0,end=n*m-1;
+
+//         while(start<=end){
+
+
+//             int mid = (start+end)/2;
+
+//             int row=mid/m;
+//             int col=mid%m;
+
+//             if(matrix[row][col]==target) return true;
+
+//             else if(matrix[row][col]<target) start=mid+1;
+//             else end=mid-1;
+//         }
+//         return false;
+//     }
+// };
